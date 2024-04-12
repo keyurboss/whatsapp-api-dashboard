076f31dd6148bd55012667c115c9062f7449b25e
Keyur Shah <keyurshah3939@gmail.com>
2024-04-12 06:30:50 +0530

    Some Things Added

--- apps/whatsapp-user-dashboard/src/app/app.routes.ts
+++ apps/whatsapp-user-dashboard/src/app/app.routes.ts
@@ -1,3 +1,13 @@
+import { inject } from '@angular/core';
 import { Route } from '@angular/router';
+import { LoginGuard } from './guards/login.guard';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: 'login',
+    canActivate: [inject(LoginGuard).isLogin()],
+    canMatch: [inject(LoginGuard).isLogin()],
+    loadComponent: () =>
+      import('./Login/Login.component').then((a) => a.LoginComponent),
+  },
+];
--- apps/whatsapp-user-dashboard/src/app/guards/login.guard.ts
+++ apps/whatsapp-user-dashboard/src/app/guards/login.guard.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LoginGuard {
-  constructor() {}
+  constructor(private router:Router) {}
 
   isLogin() {
     return true;   
